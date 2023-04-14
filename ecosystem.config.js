add9c059c3bc1d841f158d971d83fe8772009702
pinnnguyen <pinnguyen0597@gmail.com>
2023-04-14 13:29:30 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       env_production: {
         PORT: 3000,
         NODE_ENV: 'production',
-        OPEN_AI_API_KEY: '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',
+        OPEN_AI_API_KEY: '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',
         MONGO_URL: 'mongodb+srv://cuongnd:jBtjX9WYuM4WGzdZ@cluster0.ptgdomn.mongodb.net/master-pen',
         FEEDBACK_URL: 'https://dytr.canny.io/feedback',
       },
