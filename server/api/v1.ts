fa250d8bca29b5659df0048d0038ef2d66d4fa30
pinnnguyen <pinnguyen0597@gmail.com>
2023-04-14 13:57:17 +0700

    Update v1.ts

--- server/api/v1.ts
+++ server/api/v1.ts
@@ -15,8 +15,7 @@ export default defineEventHandler(async (event) => {
   }
 
   console.log('content', content)
-  const API_KEY = process.env.OPEN_AI_API_KEY
-  console.log('API KEY: ', API_KEY)
+  console.log('API KEY: ', process.env.OPEN_AI_API_KEY)
 
   const stream = await OpenAI(
     'chat',
@@ -28,7 +27,7 @@ export default defineEventHandler(async (event) => {
       stream: true,
     },
     {
-      apiKey: API_KEY,
+      apiKey: process.env.OPEN_AI_API_KEY,
     },
   )
 
