489bdc5d8e7a8ba8088a02c5a4f0f15f0102905f
pinnnguyen <pinnguyen0597@gmail.com>
2023-04-14 11:58:30 +0700

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,14 +7,14 @@ module.exports = {
       env: {
         PORT: 3000,
         NODE_ENV: 'development',
-        OPEN_AI_API_KEY: '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',
+        OPEN_AI_API_KEY: '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',
         MONGO_URL: 'mongodb+srv://cuongnd:jBtjX9WYuM4WGzdZ@cluster0.ptgdomn.mongodb.net/master-pen',
         FEEDBACK_URL: 'https://dytr.canny.io/feedback',
       },
       env_production: {
         PORT: 3000,
         NODE_ENV: 'production',
-        OPEN_AI_API_KEY: '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',
+        OPEN_AI_API_KEY: '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',
         MONGO_URL: 'mongodb+srv://cuongnd:jBtjX9WYuM4WGzdZ@cluster0.ptgdomn.mongodb.net/master-pen',
         FEEDBACK_URL: 'https://dytr.canny.io/feedback',
       },
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,11 +21,12 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@nuxt/image-edge',
     '@vueuse/nuxt',
-    '@vite-pwa/nuxt',
     '@nuxtjs/google-fonts',
+    '@kevinmarrec/nuxt-pwa',
   ],
   css: ['@/assets/styles/global.css'],
   app: {
+    // buildAssetsDir: '/assets/',
     keepalive: true,
     head: {
       viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
@@ -36,17 +37,29 @@ export default defineNuxtConfig({
         { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
         { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
       ],
-      script: [
-        { src: 'https://app.lemonsqueezy.com/js/lemon.js', defer: true },
-      ],
       meta: [
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-        // open graph social image
-        { property: 'og:title', content: 'Magic Pen' },
-        { property: 'og:description', content: 'Write Anything in seconds just like Magick. Powered by AI, let me be your intelligent writing assistant.' },
-        { property: 'og:type', content: 'website' },
-        { property: 'og:site_name', content: 'Magic Pen' },
+        {
+          name: 'copyright',
+          content: 'Dytr',
+        },
       ],
+    },
+  },
+  pwa: {
+    manifest: {
+      name: 'Dyrt',
+      short_name: 'Dyrt',
+      start_url: '/',
+      lang: 'en',
+    },
+    meta: {
+      title: 'Dyrt - Best AI Writer, Content Generator & Writing Assistant',
+      favicon: true,
+      theme_color: '#FFFFFF',
+      mobileApp: true,
+      mobileAppIOS: true,
+      author: 'Bon Studio',
+      description: 'Write Anything in seconds just like Magick. Powered by AI, let me be your intelligent writing assistant.',
     },
   },
 })
